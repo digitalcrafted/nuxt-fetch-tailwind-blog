a3147af6a1748f45eac6e33fb8941195ea07ddaa
Francis Onwumere <fran@mobnia.com>
2021-11-30 15:03:44 +0100

    add eslint and babel for cross browser compatibility

--- nuxt.config.js
+++ nuxt.config.js
@@ -26,7 +26,10 @@ export default {
   components: true,
 
   // Modules for dev and build (recommended): https://go.nuxtjs.dev/config-modules
-  buildModules: [],
+  buildModules: [
+    // https://go.nuxtjs.dev/tailwindcss
+    '@nuxtjs/tailwindcss'
+  ],
 
   // Modules: https://go.nuxtjs.dev/config-modules
   modules: [],
